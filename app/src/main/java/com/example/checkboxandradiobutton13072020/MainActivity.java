9c0213cb097603c708265bb7f78439d525b6653e
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-07 18:20:05 +0700

    finish

--- app/src/main/java/com/example/checkboxandradiobutton13072020/MainActivity.java
+++ app/src/main/java/com/example/checkboxandradiobutton13072020/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
     CheckBox mCbAndroid,mCbIos,mCbPhp;
+    RadioGroup mRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +20,28 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         mCbAndroid = findViewById(R.id.checkboxAndroid);
         mCbIos = findViewById(R.id.checkboxIos);
         mCbPhp = findViewById(R.id.checkboxPhp);
+        mRadioGroup = findViewById(R.id.radioGroup);
 
         mCbAndroid.setOnCheckedChangeListener(this);
         mCbIos.setOnCheckedChangeListener(this);
         mCbPhp.setOnCheckedChangeListener(this);
+
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.radioFlutter :
+                        Toast.makeText(MainActivity.this, "Choose flutter", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioReact :
+                        Toast.makeText(MainActivity.this, "Choose React", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioHybrid :
+                        Toast.makeText(MainActivity.this, "Choose hybrid", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 
     @Override
